487a49deaefbe53898c1c36a1ba0353d3459bc7b
orlova.julia.ivan@gmail.com <orlova.julia.ivan@gmail.com>
2023-02-23 12:58:17 +0300

    Добавьте файлы проекта.

---
+++ Interface/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith=new ArithProgression();
+            arith.setStart(10);
+            arith.setStep(2);
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            arith.reset();
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.ReadKey();
+
+            GeomProgression geom= new GeomProgression();
+            geom.setStart(10);
+            geom.setStep(2);
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            geom.reset();
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.ReadKey();
+        }
+    }
+}
